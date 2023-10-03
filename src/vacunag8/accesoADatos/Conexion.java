4693594e65b098d2c0809602c2ca34a6f1bc46b7
manuvillega <manueldxt@gmail.com>
2023-10-03 20:07:33 -0300

    Update Conexion.java

--- src/vacunag8/accesoADatos/Conexion.java
+++ src/vacunag8/accesoADatos/Conexion.java
@@ -7,38 +7,32 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-
-
 public class Conexion {
 
-    private static final String URL="jdbc:mariadb://localhost/";
-    private static final String DB="vacunar23";
-    private static final String USUARIO="root";
-    private static final String PASSWORD="";
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "vacunar23";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
     private static Connection connection;
-    
-    private Conexion(){}
-    
-    public static Connection getConnection(){
-        if(connection==null){
+
+    private Conexion() {
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
             try {
                 Class.forName("org.mariadb.jdbc.Driver");
-                connection=DriverManager.getConnection(URL+DB, USUARIO, PASSWORD);
-                 JOptionPane.showMessageDialog(null, "conectado");
-                
+                connection = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+                JOptionPane.showMessageDialog(null, "conectado");
+
             } catch (ClassNotFoundException ex) {
                 JOptionPane.showMessageDialog(null, "Error al cargar los Driver");
             } catch (SQLException ex) {
-                JOptionPane.showMessageDialog(null, "Error al conectarse a la Base de Datos");  
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la Base de Datos");
             }
         }
         return connection;
-        
-        
+
     }
-     
-    
-    
-    
-    
+
 }
