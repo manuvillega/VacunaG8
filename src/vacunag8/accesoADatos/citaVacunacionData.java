e98710d3eb84a5d71d707f320c94dc1ef1ebfdb2
Metal <guillermomaxi2009@gmail.com>
2023-10-05 20:29:54 -0300

    citaVacunacionData

---
+++ src/vacunag8/accesoADatos/citaVacunacionData.java
@@ -0,0 +1,75 @@
+
+package vacunag8.accesoADatos;
+
+import Entidades.citaVacunacion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class citaVacunacionData {
+    private Connection conexion;
+
+    public citaVacunacionData(Connection conexion) {
+        this.conexion = conexion;
+    }
+    
+    public void CrearCita(citaVacunacion cita){
+        String sql = "INSERT INTO citavacunacion (persona, codRefuerzo, fechaHoraCita, centroVacunacion)"
+                + "VALUES(?, ?, ?, ?, ?)";
+        try(PreparedStatement preparedStatement = conexion.prepareStatement(sql)){
+            preparedStatement.setInt(1, cita.getPersona().getDNI());
+            preparedStatement.setInt(2, 1);
+            preparedStatement.setString(3, cita.getFechaHoraCita());
+            preparedStatement.setString(4, cita.getCentroVacunacion().getNombre());
+        } catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "No fue posible conectar para crear cita"+e);
+        }
+    }
+    
+    public void ModificarCita(citaVacunacion cita){
+        String sql = "UPDATE citavacunacion SET fechaHoraCita = ? WHERE persona = ?";
+        
+        try(PreparedStatement preparedStatement = conexion.prepareStatement(sql)){
+            preparedStatement.setString(1, cita.getFechaHoraCita());
+            preparedStatement.setInt(2, cita.getPersona().getDNI());
+        } catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "No fue posible conectar para modificar cita"+e);
+        }
+    }
+    
+    public void VacunaAplicada(citaVacunacion cita){
+        String sql = "UPDATE citavacunacion SET fechaHoraColoca = ?, dosis = ? WHERE persona = ?";
+        LocalDateTime fecha = cita.getFechaHoraVacunacion();       
+        try(PreparedStatement preparedStatement = conexion.prepareStatement(sql)){
+            Date nuevaDate = Date.valueOf(fecha.toLocalDate());
+            preparedStatement.setDate(1, nuevaDate);
+            preparedStatement.setInt(2, cita.getDosis().getNroSerieDosis());
+            preparedStatement.setInt(3, cita.getPersona().getDNI());
+        } catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "No fue posible conectar para modificar cita"+e);
+        }
+    }
+    
+   public LocalDateTime ProximaCita(citaVacunacion cita){
+       String sql = "UPDATE citavacunacion SET fechaHoraCita = ? WHERE persona = ?";
+       LocalDateTime fecha = cita.getFechaHoraVacunacion();
+       LocalDateTime fechaNueva = fecha.plusDays(42);
+       try(PreparedStatement preparedStatement = conexion.prepareStatement(sql)){
+            Date nuevaDate = Date.valueOf(fechaNueva.toLocalDate());
+            preparedStatement.setDate(1, nuevaDate);
+            preparedStatement.setInt(2, cita.getPersona().getDNI());
+        } catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "No fue posible conectar para modificar cita"+e);
+        }
+       return fechaNueva;
+   }
+    
+   public List<citaVacunacion> listarCitasCumplidas(){
+       String sql = "SELECT ";
+        return null;
+   }
+}
