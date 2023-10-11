b50ba61a61fed2219c42fd770857e3998681c1f7
Gonza <103911012+D3Po1@users.noreply.github.com>
2023-10-11 18:18:56 -0300

    Clase main

--- src/vacunag8/VacunaG8.java
+++ src/vacunag8/VacunaG8.java
@@ -1,33 +1,112 @@
 package vacunag8;
 
-import Entidades.Ciudadano;
-import java.sql.Connection;
-import vacunag8.accesoADatos.CiudadanoData;
-import vacunag8.accesoADatos.Conexion;
+/**
+ *
+ * @author Gonz@_
+ */
 
+import Entidades.Laboratorio;
+import java.sql.Connection;
+import java.util.List;
+import accesoADatos.Conexion;
+import accesoADatos.LaboratorioData;
+import javax.swing.JOptionPane;
+import utilidades.ValidarCUIT;
 
 public class VacunaG8 {
-
 
     public static void main(String[] args) {
 
         Connection conexion = Conexion.getConexion();
-        
+
+        /* ##################################################################################
+                //##CIUDADANO##
+                    // instancias
+                 // pide y obtiene los datos ingresados para crear el Ciudadano /despiues se reutilizaràn en la GUI
+        int dni = Integer.parseInt(JOptionPane.showInputDialog("Ingrese DNI:"));
+        String nombre = JOptionPane.showInputDialog("Ingrese nombre:");
+        String email = JOptionPane.showInputDialog("Ingrese email:");
+        String telefono = JOptionPane.showInputDialog("Ingrese teléfono:");
+        String profesion = JOptionPane.showInputDialog("Ingrese profesión:");
+        String patologia = JOptionPane.showInputDialog("Ingrese patología:");
+
         CiudadanoData ciudadanoData = new CiudadanoData(conexion);
 
-        Ciudadano ciudadano1 = new Ciudadano (23456177,"Juan Perez", "JPerez@gmail.com","3578882228", "asma", "Policia");
-        
-//        ciudadanoData.insertarCiudadano(ciudadano1);
+        // crear el objeto Ciudadano con los datos ingresados
+        Ciudadano ciudadano = new Ciudadano(dni, nombre, email, telefono, profesion);
+        ciudadano.setPatologia(patologia);
 
-        
-        ciudadano1.setPatologia("hepatitis");
-        ciudadanoData.actualizarCiudadano(ciudadano1);
-        
+        //metodo  insertar ciudadano
+        ciudadanoData.insertarCiudadano(ciudadano);
+
+        // Modificar un atributo-variable
+        ciudadano.setPatologia("hepatitis");
+
+        // Actualizar-Modificar ciudadano
+        ciudadanoData.actualizarCiudadano(ciudadano);
+
+        // Listar ciudadanos
+        List<Ciudadano> ciudadanos = ciudadanoData.listarCiudadanos();
+        System.out.println("Listar ciudadanos:");
+        for (Ciudadano ciudad : ciudadanos) {
+            System.out.println(ciudad);
+        }
+
+        // borrar- eliminar ciudadano
+        ciudadanoData.borrarCiudadano(ciudadano);
     }
-    
-}
-
-
-
-       
+         ###################################################################################*/
         
+        
+                //##LABORATORIO##
+        // instancias
+         LaboratorioData laboratorioData = new LaboratorioData(conexion);
+
+        // pide y obtiene los datos ingresados para crear el Laboratorio /despiues se reutilizaràn en la GUI
+        String cuitCompleto = JOptionPane.showInputDialog("Ingrese el CUIT completo (11 dígitos):");
+        String nombreLaboratorio = JOptionPane.showInputDialog("Ingrese el nombre del laboratorio:");
+        String pais = JOptionPane.showInputDialog("Ingrese el país:");
+        String direccion = JOptionPane.showInputDialog("Ingrese la dirección:");
+
+//        // validacion de apoyo
+//        if (!ValidarCUIT.esUnCUITValido(cuitCompleto)) {
+//            String cuitSinUltimoDigito = cuitCompleto.substring(0, 10);
+//            int digitoVerificadorCalculado = ValidarCUIT.calcularDigitoVerificador(cuitSinUltimoDigito);
+//            int option = JOptionPane.showConfirmDialog(
+//                    null,
+//                    "El CUIT ingresado es incorrecto. ¿Desea utilizar el CUIT corregido: " + cuitSinUltimoDigito + digitoVerificadorCalculado + "?",
+//                    "Confirmación",
+//                    JOptionPane.YES_NO_OPTION
+//            );
+//            if (option == JOptionPane.YES_OPTION) {
+//                cuitCompleto = cuitSinUltimoDigito + digitoVerificadorCalculado;
+//            } else {
+//                // cierra sale el programa si el usaario no quiere el CUIT correcto 
+//                System.exit(0);
+//            }
+//        }
+
+        // inserta-crear un nuevo laboratorio
+        Laboratorio laboratorio = new Laboratorio(cuitCompleto, nombreLaboratorio, pais, direccion);
+        laboratorioData.agregarLaboratorio(laboratorio);
+
+        // obtiene un laboratorio por CUIT
+        Laboratorio laboratorioObtenido = laboratorioData.obtenerLaboratorioPorCUIT("20344545552");
+        System.out.println("Laboratorio obtenido: " + laboratorioObtenido);
+
+        // actualiza- modifica un laboratorio
+        laboratorioObtenido.setNomLaboratorio("Laboratorio 3");
+        laboratorioData.actualizarLaboratorio(laboratorioObtenido);
+
+        // borra-eliminar un laboratorio
+        //laboratorioData.eliminarLaboratorio("...");
+        
+        // listar laboratorios
+        List<Laboratorio> laboratorios = laboratorioData.listarLaboratorios();
+        System.out.println("Lista de laboratorios: " + laboratorios);
+
+        // cerrar la conexión al finalizar
+        Conexion.desconectar();
+    }
+
+}
