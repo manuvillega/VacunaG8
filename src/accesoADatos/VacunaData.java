a0ffa757e49674a7dc7eadf7a05cdff8a18ed69b
manuvillega <141779029+manuvillega@users.noreply.github.com>
2023-10-30 12:51:45 -0300

    Add files via upload

--- src/accesoADatos/VacunaData.java
+++ src/accesoADatos/VacunaData.java
@@ -6,121 +6,120 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+
 
 public class VacunaData {
 
-    private Connection conexion;
-    
-    public VacunaData(Connection conexion){
-        this.conexion=conexion;
-    }
+    private Connection conexion = null;
 
-    
-    public void cargarVacunas(Vacuna cargar){
-        
-        String sql = "INSERT INTO vacuna (nroSerieDosis, marca, medida, fechaCaduca, colocada) VALUES (?, ?, ?, ?, ?)";
-        
-        try {
-            PreparedStatement ps = conexion.prepareStatement(sql);
-            
-            ps.setInt(1, cargar.getNroSerieDosis());
-            ps.setString(2, cargar.getMarca());
-            ps.setDouble(3, cargar.getMedida());
-            ps.setDate(4, (Date) cargar.getFechaCaduca());            
-            ps.setBoolean(5, cargar.isColocada());
-            ps.executeUpdate();
-            System.out.println("Se han cargado vacunas");
-            
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "error de conexion con tabla vacuna 1");
-        }  
-        
+    public VacunaData() {
+        conexion = Conexion.getConexion();
     }
+//GUARDAR VACUNA  
+        public void cargarVacunas(Vacuna cargar){
+
+            String sql = "INSERT INTO vacuna (nroSerieDosis, marca, medida, fechaCaduca, colocada) VALUES (?, ?, ?, ?, ?)";
+
+            try {
+                PreparedStatement ps = conexion.prepareStatement(sql);
+                ps.setInt(1, cargar.getNroSerieDosis());
+                ps.setString(2, cargar.getMarca());
+                ps.setDouble(3, cargar.getMedida());
+                ps.setDate(4, (Date) cargar.getFechaCaduca()); 
+                ps.setBoolean(5, cargar.isColocada());
+                ps.executeUpdate();
+
+                JOptionPane.showMessageDialog(null, "Se ha cargado una Vacuna");
+                ps.close();
+
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "error de conexion con tabla vacuna");
+            }    
+        }
+//MODIFICAR VACUNA  
     public void modificarStockVacunas(Vacuna vacuna){
-    
+        
         String sql = "UPDATE vacuna SET marca=?, medida=?, fechaCaduca=?, colocada=? WHERE nroSerieDosis=?";
-               
+        PreparedStatement ps = null;       
         try {  
-            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps = conexion.prepareStatement(sql);
             ps.setString(1, vacuna.getMarca());
             ps.setDouble(2, vacuna.getMedida());
-            ps.setDate(3, (Date) vacuna.getFechaCaduca());
+            ps.setDate(3, (Date)(vacuna.getFechaCaduca()));
             ps.setBoolean(4, vacuna.isColocada());
             ps.setInt(5, vacuna.getNroSerieDosis());
-            ps.executeUpdate();
-            System.out.println("se ha modificado vacuna");
+            int exito = ps.executeUpdate();
             
+            if(exito==1){
+               JOptionPane.showMessageDialog(null, "Has modificado una Vacuna "); 
+            }else{
+               JOptionPane.showMessageDialog(null, "La Vacuna no existe");  
+            }
+                        
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "error de conexion con tabla vacuna 2");
+            JOptionPane.showMessageDialog(null, "error de conexion con tabla vacuna");
         }    
     }
-    
+//ELIMINAR VACUNA LOGICAMENTE    
     public void eliminarVacuna (Vacuna borrar){
         
-        String sql = "UPDATE vacuna SET colocada = 0 WHERE nroSerieDosis = ? ";
+        String sql = "UPDATE vacuna SET colocada = 1 WHERE nroSerieDosis = ? ";
         
         try {
             PreparedStatement ps = conexion.prepareStatement(sql);
             ps.setInt(1, borrar.getNroSerieDosis());
-            ps.executeUpdate();
-            System.out.println("Se ha eliminado una vacuna");
-            
-            
+            int fila = ps.executeUpdate();
+            if(fila==1){
+              JOptionPane.showMessageDialog(null, "Se ha Eliminado una Vacuna");   
+            }
+            ps.close();            
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "error de conexion con tabla vacuna 3");
+            JOptionPane.showMessageDialog(null, "error de conexion con tabla vacuna");
         }
-    }
-
-    public Vacuna obtenerVacunaPorNroSerie(int nroSerie){
-        Vacuna vacuna = null;
-        try{
-            String sql = "SELECT * FROM vacuna WHERE nroSerieDosis = ?";
+    }    
+//BUSCAR VACUNA    
+    public Vacuna obtenerVacunaPorNroSerie(int nroSerieDosis){
+        
+        Vacuna vacuna = null;        
+        String sql = "SELECT  marca, medida, fechaCaduca, colocada FROM vacuna WHERE nroSerieDosis=?";
+        
+        
+        
+//        PreparedStatement ps = null;        
+        try{    
             PreparedStatement ps = conexion.prepareStatement(sql);
-            ps.setInt(1, nroSerie);
+            ps.setInt(1, nroSerieDosis);            
             ResultSet rs = ps.executeQuery();
-            
-            if(rs.next()){
-                int nroSerieDosis = nroSerie;
-                String marca = rs.getString("marca");
-                double medida = rs.getDouble("medida");
-                Date fechaCaduca = rs.getDate("fechaCaduca");
-                boolean colocada = rs.getBoolean("colocada");
-                vacuna = new Vacuna(nroSerieDosis, marca, medida, fechaCaduca);
+           
+            if(rs.next()){               
+                vacuna=new Vacuna();            
+        
+                vacuna.setMarca(rs.getString("marca"));
+                
+        
+                
+                vacuna.setMedida(rs.getDouble("medida"));
+                
+        
+        
+                vacuna.setFechaCaduca(rs.getDate("fechaCaduca"));
+                
+
+                vacuna.setColocada(rs.getBoolean("colocada")); 
+                
+                
+                JOptionPane.showMessageDialog(null,vacuna);    //para ver si toma la vacuna de la base de datos
+        
+            }else{
+               JOptionPane.showMessageDialog(null,"la vacuna no existe");                
             }
             ps.close();
-            
         } catch(SQLException ex){
             JOptionPane.showMessageDialog(null, "error al acceder a la tabla Vacuna 4");
         }
         return vacuna;
     }
-    
-//    public Vacuna obtenerVacunaPorMarca(String marca){
-//        Vacuna vacuna = null;
-//        try{
-//            String sql = "SELECT * FROM vacuna WHERE marca = ?";
-//            PreparedStatement ps = conexion.prepareStatement(sql);
-//            ps.setString(1, marca);
-//            ResultSet rs = ps.executeQuery();
-//            
-//            if(rs.next()){
-//                int nroSerieDosis = nroSerie;
-//                String marca = rs.getString("marca");
-//                double medida = rs.getDouble("medida");
-//                Date fechaCaduca = rs.getDate("fechaCaduca");
-//                boolean colocada = rs.getBoolean("colocada");
-//                vacuna = new Vacuna(nroSerieDosis, marca, medida, fechaCaduca);
-//            }
-//            ps.close();
-//            
-//        } catch(SQLException ex){
-//            JOptionPane.showMessageDialog(null, "error al acceder a la tabla Vacuna 4");
-//        }
-//        return vacuna;
-//    }
 }    
             
             
